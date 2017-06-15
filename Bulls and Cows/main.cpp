f32edd5a3200170a7581c23b1e1c62dcf1cd203b
dalei <dalei@DESKTOP-MESCBA0>
2017-06-15 17:51:59 +0200

    Finished game main structure.

--- Bulls and Cows/main.cpp
+++ Bulls and Cows/main.cpp
@@ -4,33 +4,59 @@
 using namespace std;
 
 void PrintIntro();
-void GetGuessAndPrintBack();
+string GetGuess();
+void PlayGame();
+bool AskToPlayAgain();
 
 //start of the aplication
 int main()
 {
-	PrintIntro();
-	GetGuessAndPrintBack();
-
-	return 0;
+	bool bPlayAgain;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+		cout << endl;
+	} 
+	while (bPlayAgain);
+	
+	return 0; //end of the applications
 }
 
-//introduce the game
 void PrintIntro() 
 {
+	//introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 	cout << endl;
 }
 
-//get a guess from the player
-void GetGuessAndPrintBack() 
+string GetGuess() 
 {
+	//get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
+	return Guess;
+}
 
-	//repeat the guess back to the player
-	cout << "Your guess was: " << Guess << endl;
+void PlayGame()
+{
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 0;NUMBER_OF_TURNS > count; count++) {
+		string Guess = GetGuess();
+		//repeat the guess back to the player
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again? (y/n) ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y' || Response[0] == 'Y'); //exit the application
 }
